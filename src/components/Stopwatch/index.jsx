2239ed0faf55d44a71715ea1098d2f7396207c35
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-12-04 02:51:33 -0300

    [WEB] use-effect-studies - update: creating components for the useEffect studies

---
+++ src/components/Stopwatch/index.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function Stopwatch() {
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(oldTime => oldTime + 1)
+    }, 1000)
+    
+    return () => clearInterval(intervalID)
+  }, [])
+
+  return (
+    <div>
+      <p>Time: {time}</p>
+    </div>
+  )
+}
+
+export default Stopwatch
---
+++ src/components/UseEffectComponent/index.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import Stopwatch from './components/Stopwatch';
+
+function UseEffectComponent() {
+
+  const [names, setNames] = useState([])
+  
+  useEffect(() => {
+    fetch("/names.json")
+    .then((res) => res.json())
+    .then((data) => setNames(data))
+    .catch((e) => console.error('Error Message (catch): ', e))
+  }, [])
+  
+  const [selectedName, setSelectedName] = useState(null)
+  const [selectedNameDetails, setSelectedNameDetails] = useState(null)
+
+  const handleSelectedNameChange = (name) => {
+    fetch(`/${name}.json`)
+      .then((res) => res.json())
+      .then((data) => setSelectedNameDetails(data))
+      .catch((e) => console.error('Error Message (catch): ', e))
+  }
+  return (
+    <div>
+      <Stopwatch/>
+      <div>
+        {names.map((name, key) => (
+          <button 
+            key={key}
+            onClick={() => handleSelectedNameChange(name)}
+          >
+            {name}
+          </button>
+        ))}
+      </div>
+      <div>{JSON.stringify(selectedName)}</div>
+      <div>Selected Name Details: {JSON.stringify(selectedNameDetails)}</div>
+    </div>
+  )
+}
+
+export default UseEffectComponent
