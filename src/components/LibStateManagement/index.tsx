7adab892f5ddd37d1520f40d42269795880e2469
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-12-18 15:41:18 -0300

    [WEB] styling-the-components - refactor: created header

--- src/components/LibStateManagement/index.tsx
+++ src/components/LibStateManagement/index.tsx
@@ -49,7 +49,7 @@ export default function LibStateManagement() {
     <Tabs
       style={{
         position: 'absolute',
-        top: 0,
+        top: 40,
         bottom: 0,
         left: 0,
         right: 0,
